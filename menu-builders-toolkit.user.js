f3f2e1d4b9c4930cb193e01a81a4edd4ad121882
Jonatas <jonatas.dias@estudante.ufjf.br>
2025-12-10 09:51:36 -0300

    chore: bump version to 0.2.2

--- menu-builders-toolkit.user.js
+++ menu-builders-toolkit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Menu Builders' Toolkit
 // @namespace    https://github.com/jooj211/menu-builders-toolkit
-// @version      0.2.1
+// @version      0.2.2
 // @description  Helper tools for Popmenu menu builders (modifier tags, etc.)
 // @author       Jonatas Dias
 // @match        https://my.popmenu.com/*
